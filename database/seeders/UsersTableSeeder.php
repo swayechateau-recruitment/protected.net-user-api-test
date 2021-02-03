cb5cf84bd1107af6a45011b408cadee842959e6a
Swaye Chateau <swaye@kitechltd.com>
2021-02-03 12:29:52 +0000

    fixed typo

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
-class UserSeeder extends Seeder
+class UsersTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
