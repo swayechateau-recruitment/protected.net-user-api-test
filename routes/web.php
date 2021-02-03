bc591ee5a6c1d2fda6e07c77e98b3238ff7a1dad
Swaye Chateau <swaye@kitechltd.com>
2021-02-03 12:10:36 +0000

    updated exceptions handler to return json

--- routes/web.php
+++ routes/web.php
@@ -2,29 +2,31 @@
 
 /** @var \Laravel\Lumen\Routing\Router $router */
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
 
-// app docs
+/**
+ * Application Routes
+ *  */
+
+// App docs
 $router->get('/', 'AppController@index');
-// app install
+// App install
 $router->get('/install', 'AppController@install');
-// app search queries - only users return for now
+// App search queries - only users return for now
 $router->get('/search', 'AppController@search');
 
-// users crud routes
+/**
+ * Users crud routes
+ *  */
+// Get all Users
 $router->get('/users', 'UserController@index');
 // Add a user
 $router->post('/users', 'UserController@create');
-
+// Get a user
 $router->get('/users/{id}', 'UserController@show');
+// Update a user - only name supported currently
 $router->put('/users/{id}', 'UserController@update');
+// Delete a user
 $router->delete('/users/{id}', 'UserController@destroy');
+
+// Toggle a users darkmode setting
+$router->get('/users/{id}/tdm', 'UserController@toggleDarkMode');
