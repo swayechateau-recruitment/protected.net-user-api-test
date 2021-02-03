d93c70dd5ccc844063ce5c381c47820d021f9ab8
Swaye Chateau <swaye@kitechltd.com>
2021-02-03 11:50:41 +0000

    added users migration

---
+++ database/migrations/2021_02_03_114931_create_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('username', 20)->unique();
+            $table->string('first_name', 50);
+            $table->string('last_name', 50);
+            $table->boolean('dark_mode')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
